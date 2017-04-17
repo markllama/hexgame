fd210da8bdef2777c90ae989562a2bf376a73c5c
Mark Lamourine <markllama@gmail.com>
2017-04-16 20:04:50 -0400

    added hextant and Hextant()

--- hexmap/hexvector.go
+++ hexmap/hexvector.go
@@ -5,6 +5,59 @@ type HexVector struct {
 	hy int
 }
 
+var ORIGIN = HexVector{0, 0}
+var UNIT = []HexVector{
+	HexVector{ 0, -1},
+	HexVector{ 1,  0},
+	HexVector{ 1,  1},
+	HexVector{ 0,  1},
+	HexVector{-1,  0},
+	HexVector{-1, -1},
+	HexVector{ 0, -1},
+}
+
+/*
+# -1 = 0
+#  0 = 1
+#  1 = 2
+
+# on axis
+#  0-1-1 0  100  0b010000
+#  1 0-1 1  210  0b100100
+#  1 1 0 2  221  0b101001
+#  0 1 1 3  122  0b011010
+# -1 0 1 4  012  0b000110
+# -1-1 0 5  001  0b000001
+
+# off axis
+#  1-1-1  0 200  0b100000
+#  1 1-1  1 220  0b101000
+#  1 1 1  2 222  0b101010
+# -1 1 1  3 022  0b001010
+# -1-1 1  4 002  0b000010
+# -1-1-1  5 000  0b000000
+*/
+
+var hextant = map[int]int{
+	// origin
+	0111: 0,
+	
+	// on axis
+	0100: 0,
+	0210: 1,
+	0221: 2,
+	0122: 3,
+	0012: 4,
+	0001: 5,
+
+	// off axis
+	0200: 0,
+	0220: 1,
+	0222: 2,
+	0022: 3,
+	0002: 4,
+	0000: 5,
+}
 
 // Accessor functions
 func (hv HexVector) Hx() (int) {
@@ -15,8 +68,12 @@ func (hv HexVector) Hy() (int) {
 	return hv.hy
 }
 
-func (hv HexVector ) Hz() (int) {
+func (hv HexVector) Hz() (int) {
 	return hv.hy - hv.hx
+}
+
+func (hv0 HexVector) Equal(hv1 HexVector) (bool) {
+	return hv0.Hx() == hv1.Hx() && hv0.Hy() == hv1.Hy()
 }
 
 func (hv0 HexVector) Add(hv1 HexVector) (HexVector) {
@@ -28,23 +85,13 @@ func (hv0 HexVector) Sub(hv1 HexVector) (HexVector) {
 }
 
 func abs(i int) (a int) {
-	if i == 0 {
-		a = 0
-	} else {
-		a = i * (i / i)
-	}
-	return 
+	if i < 0 {	a = -i	} else { a = i	}
+	return
 }
 
 func max3(i, j, k int) (a int) {
-	if i < j {
-		a = j
-	} else {
-		a = i
-	}
-	if a < k {
-		a = k
-	}
+	if i < j { a = j} else { a = i }
+	if a < k { a = k }
 	return
 }
 
@@ -54,4 +101,18 @@ func (hv HexVector) Length() (l int) {
 
 func (hv0 HexVector) Distance(hv1 HexVector) (int) {
 	return  abs(hv0.Sub(hv1).Length())
+}
+
+// hx / abs(hx)
+
+func (hv HexVector) Hextant() (int) {
+
+	var m, rx, ry, rz int
+	if hv.Hx() == 0 {	rx = 1 } else { rx = hv.Hx() / abs(hv.Hx()) + 1}		
+	if hv.Hy() == 0 {	ry = 1 } else { ry = hv.Hy() / abs(hv.Hy()) + 1}
+	if hv.Hz() == 0 {	rz = 1 } else { rz = hv.Hz() / abs(hv.Hz()) + 1}
+	m = (rx << 6) + (ry << 3) + rz
+	//return m
+
+	return hextant[m]
 }
--- hexmap/hexvector_test.go
+++ hexmap/hexvector_test.go
@@ -17,6 +17,20 @@ func TestConstructors(t *testing.T) {
 	}
 }
 
+
+func TestEqual(t *testing.T) {
+	var o = HexVector{0, 0}
+
+	if o.Equal(ORIGIN) != true {
+		t.Error("HexVector.Equal() = origin mismatch")
+	}
+
+	o = HexVector{0, -1}
+	if o.Equal(UNIT[0]) != true {
+		t.Error("HexVector.Equal() = UNIT[0] mismatch")
+	}
+}
+
 func TestAdd(t *testing.T) {
 	var hv0 = HexVector{hx: 3, hy: 5}
 	var hv1 = HexVector{hx: 4, hy: -6}
@@ -44,3 +58,107 @@ func TestSub(t *testing.T) {
 		t.Error("HexVector.Sub - Hy() expected: 10 : actual: " + fmt.Sprintf("%d", hv2.Hy()))
 	}
 }
+
+
+func TestAbs(t *testing.T) {
+	if abs(4) != 4 {
+		t.Error("Hexvector.abs(): expected: 4, actual: " + fmt.Sprintf("%d", abs(4)))
+	}
+
+	if abs(-4) != 4 {
+		t.Error("Hexvector.abs(): expected: 4, actual: " + fmt.Sprintf("%d", abs(-4)))
+	}
+}
+
+func TestMax3(t *testing.T) {
+	var a =  max3(3, 2, 1)
+	if a != 3 {
+		t.Error("HexVector.max3(): expected first: 3, actual: " + fmt.Sprintf("%d", a))	
+	}
+
+	a =  max3(2, 3, 1)
+	if a != 3 {
+		t.Error("HexVector.max3(): expected second: 3, actual: " + fmt.Sprintf("%d", a))	
+	}
+
+
+	a =  max3(2, 1, 3)
+	if a != 3 {
+		t.Error("HexVector.max3(): expected third: 3, actual: " + fmt.Sprintf("%d", a))	
+	}
+
+	a = max3(3, 3, 3)
+	if a != 3 {
+		t.Error("HexVector.max3(): expected all: 3, actual: " + fmt.Sprintf("%d", a))	
+	}
+
+	a = max3(-3, -3, -3)
+	if a != -3 {
+		t.Error("HexVector.max3(): expected all: -3, actual: " + fmt.Sprintf("%d", a))	
+	}
+}
+
+func TestLength(t *testing.T) {
+	var hx0 = HexVector{hx: 3, hy: 2}
+	var hx1 = HexVector{hx: 4, hy: -3}
+	
+	var l = hx0.Length()
+	if l != 3 {
+		t.Error("HexVector.Length() expect: 3, actual: " + fmt.Sprintf("%d", l))
+	}
+
+	l = hx1.Length()
+	if l != 7 {
+		t.Error("HexVector.Length() expect: 7, actual: " + fmt.Sprintf("%d", l))
+	}
+}
+
+func TestDistance(t* testing.T) {
+ 	var hv0 = HexVector{3, 4}
+ 	var hv1 = HexVector{6, -3}
+
+ 	var d = hv0.Distance(hv1)
+	if d != 10 {
+		t.Error("HexVector.Distance(): expected: 10, actual: " + fmt.Sprintf("%d", d))
+	}
+}
+
+
+func TestHextant(t* testing.T) {
+
+	if hextant[0111] != 0 {
+		t.Error("hextant[0111] expect: 0, actual: " + fmt.Sprintf("%d", hextant[0111]))
+	}
+
+	if hextant[0100] != 0 {
+		t.Error("hextant[0100] expect: 0, actual: " + fmt.Sprintf("%d", hextant[0100]))
+	}
+
+	if hextant[0210] != 1 {
+		t.Error("hextant[0210] expect: 1, actual: " + fmt.Sprintf("%d", hextant[0210]))
+	}
+
+	if hextant[0221] != 2 {
+		t.Error("hextant[0221] expect: 2, actual: " + fmt.Sprintf("%d", hextant[0221]))
+	}
+
+	if hextant[0200] != 0 {
+		t.Error("hextant[0200] expect: 0, actual: " + fmt.Sprintf("%d", hextant[0200]))
+	}
+
+	
+	var h int = ORIGIN.Hextant()
+	if h != 0 {
+		t.Error("HexVector.Hextant() ORIGIN, expected: 0, actual: " + fmt.Sprintf("0%03o", h))
+	}
+
+	for i, v := range UNIT {
+		h = v.Hextant()
+		if h != i % 6 {
+			t.Error(fmt.Sprintf("HexVector.Hextant() UNIT[%d], expected: %d, actual %d",i, i % 6, h))
+		}
+	}
+
+	
+}
+
