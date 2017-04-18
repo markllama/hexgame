361c759840fc267a363c92ef2f0f6d0ca1f2d8b6
Mark Lamourine <markllama@gmail.com>
2017-04-18 16:55:54 -0400

    added json methods for hexvector to account for private variables

--- hexgame.go
+++ hexgame.go
@@ -7,12 +7,10 @@ import (
 	"gopkg.in/mgo.v2";
 	"gopkg.in/mgo.v2/bson";
 	//"net/http";
+
+	"github.com/markllama/hexgame-go/hexmap";
 )
 
-type Hexmap struct {
-	Name string
-	Size string
-}
 
 func main() {
 	fmt.Println("Hello there")
@@ -26,12 +24,12 @@ func main() {
 	session.SetMode(mgo.Monotonic, true)
 
 	c := session.DB("hexgame").C("maps")
-	err = c.Insert(&Hexmap{"Ogre", "14x22"}, &Hexmap{"WarpWar", "14x24"})
+	err = c.Insert(&hexmap.HexMap{"Ogre", hexmap.NewHexVector(22,14), &hexmap.ORIGIN}, &hexmap.HexMap{"WarpWar", hexmap.NewHexVector(22, 15), &hexmap.ORIGIN})
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	result := Hexmap{}
+	result := hexmap.HexMap{}
 	err = c.Find(bson.M{"name": "Ogre"}).One(&result)
 	if err != nil {
 		log.Fatal(err)
