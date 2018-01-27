b53c5a6bdb6d2d23a94345d5a46e63589c6f51ac
Mark Lamourine <markllama@gmail.com>
2018-01-26 20:05:07 -0500

    remove extra db connect stuff

--- server/main.go
+++ server/main.go
@@ -1,15 +1,44 @@
 package server
 
 import (
-	"github.com/markllama/hexgame/pkg/db"
+	"time"
+	"gopkg.in/mgo.v2"
 )
+
+
+//const MongoDb details
+const (
+	hosts      = "127.0.0.1:27017"
+	database   = "hexgame"
+	username   = "hexgame"
+	password   = "ragnar"
+)
+
+func Connect() (*mgo.Database) {
+	info := &mgo.DialInfo{
+		Addrs:    []string{hosts},
+		Timeout:  60 * time.Second,
+		Database: database,
+		Username: username,
+		Password: password,
+	}
+
+	session, err1 := mgo.DialWithInfo(info)
+	if err1 != nil {
+		panic(err1)
+	}
+
+	db := session.DB(database)
+	
+	return db
+}
 
 func Main() {
 
 	// Configure
 
 	// connect to database
-	database := db.Connect()
+	database := Connect()
 
 	CreateWebServer(database)
 }
