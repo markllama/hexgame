101bd42ba0d104c95891bb31302a6be04d171ec5
Mark Lamourine <markllama@gmail.com>
2018-01-27 17:16:16 -0500

    remove unused logic

--- pkg/hexmap/terrain.go
+++
@@ -1,8 +0,0 @@
-package hexmap
-
-type Terrain struct {
-	Type string
-	Name string
-	Locations []HexVector `json:"locations" bson:"locations"`
-	//Regions []Region `json:"regions" bson:"regions"`
-}
