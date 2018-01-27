497d12939b625acb847aeaaa7572016c275dc6c9
Mark Lamourine <markllama@gmail.com>
2018-01-26 22:32:38 -0500

    moved all code into main so I can find out what happens

--- server/main.go
+++ server/main.go
@@ -2,7 +2,18 @@ package server
 
 import (
 	"time"
+	"os"
+	"fmt"
+	"html"
+	"log"
+	"path"
+	"net/http"
+	"net/url"
+	"encoding/json"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"github.com/markllama/hexgame/types/hexgame"
+	"github.com/markllama/hexgame/server/handler"
 )
 
 
@@ -35,10 +46,88 @@ func Connect() (*mgo.Database) {
 
 func Main() {
 
-	// Configure
-
 	// connect to database
 	database := Connect()
 
-	CreateWebServer(database)
+	http.Handle("/game/", handler.GameServer(database))
+	http.Handle("/map/", handler.MapServer(database))
+	
+	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte("{\"foo\": \"bar\"}\n"))
+	})
+
+	http.HandleFunc("/quit", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Good Bye", html.EscapeString(r.URL.Path))
+		os.Exit(0)
+	})
+	
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type GameRef struct {
+	Name string `json:"name"`
+	URL string `json:"url"`
+}
+
+func GameHandlerFunc(s *mgo.Session) (func(http.ResponseWriter, *http.Request)) {
+
+	sc := s.Copy()
+	defer sc.Close()
+	
+	f := func(w http.ResponseWriter, r *http.Request) {
+		var g hexgame.Game
+
+		c := sc.DB("hexgame").C("games")
+		
+		w.Header().Add("Content-Type", "application/json")
+
+		_, name := path.Split(r.URL.Path)
+
+		if (name != "") {
+
+			q := c.Find(bson.M{"name": name})
+			// check for errors
+			// err := q.One(&g.Game)
+			err := q.One(&g)
+			if (err != nil) {
+				http.Error(w, fmt.Sprintf("game %s not found", name), 404)
+				return
+			}
+
+			gurl := url.URL{Scheme: "http", Host: r.Host, Path: r.URL.Path}
+			g.URL = gurl.String()
+			p, _ := json.Marshal(g)
+			//p = fmt.Sprintf("root = %s, name = %s", root, name)
+			//p = fmt.Sprintf("root = %s, no name", root)
+			w.Write(p)
+		} else {
+
+
+			var hg []hexgame.Game
+			q := c.Find(nil)
+
+			n, _ := q.Count()
+			games := make([]hexgame.Game, n)
+			q.All(&hg)
+	
+			gamerefs := make([]GameRef, len(games))
+
+			gurl := url.URL{Scheme: "http", Host: r.Host}
+		
+			for index, game := range games {
+				gurl.Path = path.Join(r.URL.Path, game.Name)
+				gamerefs[index].Name = game.Name
+				gamerefs[index].URL = gurl.String()
+			}
+		
+			jgames, _ := json.Marshal(gamerefs)
+		
+			w.Write([]byte(jgames))
+		}
+
+	}
+
+	return f
+	
 }
