f030db1eb80268c228e4678ad53864b80b2de064
Mark Lamourine <markllama@gmail.com>
2018-02-08 23:01:37 -0500

    cleaned up some terrains

--- types/terrain.go
+++ types/terrain.go
@@ -4,4 +4,5 @@ type Terrain struct {
 	Type string         `json:"type"`
 	Name string         `json:"name,omitempty"`
 	Locations []Vector  `json:"locations"`
+	// Regions []Region  `json:"regions,omitempty"`
 }
