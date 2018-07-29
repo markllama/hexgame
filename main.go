91f69cbf87cccaa865e2db29c9aa0e6e7fdaa005
Mark Lamouruine <markllama@gmail.com>
2018-07-28 23:02:22 -0400

    add db context to request

--- main.go
+++ main.go
@@ -16,22 +16,52 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	//"log"
+	"time"
 	config "github.com/markllama/hexgame/config"
+	db "github.com/markllama/hexgame/db"
 )
+
+type HttpHandlerDecorator func(http.Handler) http.Handler
 
 // the configuration is integrated from defaults, environment, config file
 // and CLI arguments in that order of precedence.
-
 func main() {
 
-	var opts = config.GetConfig()
+	opts := config.GetConfig()
 	// read configuration
 
 	// connect to database
+	session := db.Connect(&opts.MongoDBConfig)
 
+	fmt.Println(session)
 	// create api server
 
-	// create user interface server
+	myHandler1 := http.FileServer(http.Dir("./static"))
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        myHandler1,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	dbDecorator := db.CopyMongoSession(session)
 	
+	myHandler2 := dbDecorator(http.FileServer(http.Dir("./static")))
+	a := &http.Server{
+		Addr:           ":8000",
+		Handler:        myHandler2,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	go s.ListenAndServe()
+
+	// create user interface server
+	a.ListenAndServe()
+
 	fmt.Printf("hello world %s", opts)
 }
