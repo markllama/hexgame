bac334f924b0a41e866b04b011b0701c913b1f19
Mark Lamourine <markllama@gmail.com>
2018-08-23 23:08:23 -0400

    simple file server

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Hello, Goodbye")
+
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.ListenAndServe(":8000", nil)
 }
